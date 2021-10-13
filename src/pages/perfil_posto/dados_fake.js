488792b775dfb47500371215a7142e850277ca6f
Nathalia Monalisa F <nathaliamonalisa@gmail.com>
2021-10-12 21:47:05 -0300

    construção da página perfil posto de coleta

---
+++ src/pages/perfil_posto/dados_fake.js
@@ -0,0 +1,24 @@
+export default {
+    "coleta": [
+        {
+        "id": 0,
+        "contador_atual": 23,
+        "growlometro": 10
+        },
+        {
+        "id": 1,
+        "contador_atual": 12,
+        "growlometro": 6
+        },
+        {
+        "id": 2,
+        "contador_atual": 23,
+        "growlometro": 10
+        },
+        {
+        "id": 3,
+        "contador_atual": 8,
+        "growlometro": 30
+        },
+    ]
+}
---
+++ src/pages/perfil_posto/perfil_posto.js
@@ -0,0 +1,54 @@
+import dados from "./dados_fake.js";
+
+//-- Variáveis globais
+const contador = document.querySelector(".perfil-posto-btnCont")
+
+
+//-- Carregar informações iniciais na tela
+function getIDPostoColeta(){
+    // Lógica para conhecer o id do posto que está acessando o site
+    // essa identificação deverá vir da página de login
+    const fakeID = 3
+    return fakeID
+}
+
+function getDadosColetaDoPosto(){
+    const postoID = getIDPostoColeta()
+    return dados.coleta.filter(posto => posto.id == postoID)
+}
+
+function atualizarContadorAtualNaTela(){
+    const dados_coleta = getDadosColetaDoPosto()
+    contador.innerHTML = dados_coleta[0].contador_atual
+}
+
+//-- Manipulação do contador de acordo com o click do usuário
+function addClickEventListenerOnBtn(){
+    document.querySelector(".perfil-posto-btnZ").addEventListener("click", () => zerarContador())
+    document.querySelector(".perfil-posto-btnG").addEventListener("click", () => armazenarValorNoBanco())
+    document.querySelectorAll(".btn-count").forEach((btn) => btn.addEventListener("click", (e) => alterarContadorAtual(e)))
+}
+
+function alterarContadorAtual(e){
+    const tipo_btn = e.target.id
+    let valor_atual = parseInt(contador.innerHTML)
+    if (valor_atual != 0) {
+        tipo_btn == "adicao" ? valor_atual += 1 : valor_atual -= 1;   
+    } else tipo_btn == "adicao" ? valor_atual += 1 : valor_atual = 0;
+    contador.innerHTML = valor_atual
+}
+
+function zerarContador(){
+    contador.innerHTML = 0
+}
+
+function armazenarValorNoBanco(){
+    const valor_atual = parseInt(contador.innerHTML)
+    console.log(`Valor a ser armazenado no BD: ${valor_atual}`)
+    // lógica para persistir dado alterado do contador atual
+    // alert("Contador atualizado com sucesso!")    
+}
+
+//-- Ponto inicial da aplicação:
+atualizarContadorAtualNaTela()
+addClickEventListenerOnBtn()
