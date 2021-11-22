a92b8c28f239eeb62c0b30ef8ad4b67f8d4d0227
Nathalia Monalisa F <nathaliamonalisa@gmail.com>
2021-11-21 23:00:28 -0300

    Criando configuracoes bd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore, doc, addDoc, getDoc, setDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +20,24 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 
-export { db }
+// recuperar dados de uma colecao
+const querySnapshot = await getDocs(collection(db, "postos"));
+querySnapshot.forEach((doc) => {
+  if(doc.exists){
+    const dados = doc.data()
+    console.log(dados.nome)
+    console.log(dados.id)
+  }
+});
+
+
+// add dados com id automatico
+const novoPosto = {
+  id: 30,
+  nome: "Restaurante Add"
+}
+
+const docRef = await addDoc(collection(db, "postos"), novoPosto);
+console.log("Document written with ID: ", docRef.id);
+
+export { app, db }
