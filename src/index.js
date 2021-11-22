32b2e3d1af6b6d18934af4a97cdda77978781e08
Nathalia Monalisa F <nathaliamonalisa@gmail.com>
2021-11-22 18:12:40 -0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
 import { getFirestore, doc, addDoc, getDoc, setDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC5L-LPQz8x9CHofjtTRVpkXL9fp_zAbrA",
   authDomain: "eco-return.firebaseapp.com",
@@ -19,25 +15,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
-
-// recuperar dados de uma colecao
-const querySnapshot = await getDocs(collection(db, "postos"));
-querySnapshot.forEach((doc) => {
-  if(doc.exists){
-    const dados = doc.data()
-    console.log(dados.nome)
-    console.log(dados.id)
-  }
-});
-
-
-// add dados com id automatico
-const novoPosto = {
-  id: 30,
-  nome: "Restaurante Add"
-}
-
-const docRef = await addDoc(collection(db, "postos"), novoPosto);
-console.log("Document written with ID: ", docRef.id);
-
-export { app, db }
